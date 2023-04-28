4a112b54ba34237bb754cb93e606244d2b6ef109
i582 <51853996+i582@users.noreply.github.com>
2023-04-28 23:05:47 +0400

    fixed user agent

--- server/analytics.v
+++ server/analytics.v
@@ -5,5 +5,5 @@ import net.http
 pub fn (mut app Server) send_analytics(url string) {
 	ip := app.ip()
 	user_agent := app.req.header.get(http.CommonHeader.user_agent) or { '' }
-	http.post('http://localhost:8100/a', '{ "url": "${url}", "ip": "${ip}", "user_agent": ${user_agent}, "site_id": 0 }') or {}
+	http.post('http://localhost:8100/a', '{ "url": "${url}", "ip": "${ip}", "user_agent": "${user_agent}", "site_id": 0 }') or {}
 }
